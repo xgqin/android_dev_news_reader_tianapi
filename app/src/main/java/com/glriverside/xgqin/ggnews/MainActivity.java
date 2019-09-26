16768d6b3b20e190ba3c35203ac2062548f5d774
QIN Xingguo <xgqin@guet.edu.cn>
2019-09-26 17:34:45 +0800

    Using AsyncTask to fetch newslist from tianapi.

--- app/src/main/java/com/glriverside/xgqin/ggnews/MainActivity.java
+++ app/src/main/java/com/glriverside/xgqin/ggnews/MainActivity.java
@@ -1,26 +1,13 @@
 package com.glriverside.xgqin.ggnews;
 
 import android.content.Intent;
-import android.os.NetworkOnMainThreadException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
-
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
-import java.io.IOException;
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
-
-import okhttp3.Call;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
     public static String TAG = MainActivity.class.getSimpleName();
@@ -38,36 +25,6 @@ public class MainActivity extends AppCompatActivity {
             Constants.NEWS_COL7, Constants.NEWS_COL8,
             Constants.NEWS_COL10, Constants.NEWS_COL11};
 
-    private okhttp3.Callback callback = new okhttp3.Callback() {
-        @Override
-        public void onFailure(Call call, IOException e) {
-            Log.e(TAG, "Failed to connect server!");
-            e.printStackTrace();
-        }
-
-        @Override
-        public void onResponse(Call call, Response response) throws IOException {
-            if (response.isSuccessful()) {
-                final String responseData = response.body().string();
-
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Gson gson = new Gson();
-                        Type jsonType = new TypeToken<BaseResponse<List<News>>>() {}.getType();
-                        BaseResponse<List<News>> newsListResponse = gson.fromJson(responseData, jsonType);
-                        for (News news:newsListResponse.getData()) {
-                            adapter.add(news);
-                        }
-
-                        adapter.notifyDataSetChanged();
-                    }
-                });
-            } else {
-            }
-        }
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -82,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         adapter = new NewsAdapter(MainActivity.this, R.layout.list_item, newsData);
         lvNewsList.setAdapter(adapter);
 
-        refreshData(1);
+        refreshData(page);
     }
 
     private void initView() {
@@ -102,28 +59,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void refreshData(final int page) {
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                NewsRequest requestObj = new NewsRequest();
-
-                requestObj.setCol(mCols[mCurrentColIndex]);
-                requestObj.setNum(Constants.NEWS_NUM);
-                requestObj.setPage(page);
-                String urlParams = requestObj.toString();
-
-                Request request = new Request.Builder()
-                        .url(Constants.GENERAL_NEWS_URL + urlParams)
-                        .get().build();
-                try {
-                    OkHttpClient client = new OkHttpClient();
-                    client.newCall(request).enqueue(callback);
-                } catch (NetworkOnMainThreadException ex) {
-
-                    ex.printStackTrace();
-                }
-            }
-        }).start();
+        new NewsListAsyncTask(MainActivity.this, adapter)
+                .execute(new Integer[]{
+                        mCols[mCurrentColIndex],
+                        Constants.NEWS_NUM,
+                        page});
     }
 }
